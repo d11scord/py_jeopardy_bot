0ee356ca1f2ce49d0d0627a8a9ae319791d2ae71
d11scord <juliakuzkina1@gmail.com>
2021-02-11 21:54:25 +0300

    Fixed Dockerfile

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cat config/heroku_config.yaml | envsubst > config/config.yaml
+#cat config/heroku_config.yaml | envsubst > config/config.yaml
 # необходимо для того, чтобы alembic смог найти наше приложение
-export PYTHONPATH=.
+#export PYTHONPATH=.
 python main.py
