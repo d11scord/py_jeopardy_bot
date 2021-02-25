a1b19b20fcd6706fe34684b5ddfd0502ed6948bf
d11scord <juliakuzkina1@gmail.com>
2021-02-25 19:27:26 +0300

    update envs in workflows

--- app/store/database/accessor.py
+++ app/store/database/accessor.py
@@ -18,14 +18,6 @@ class PostgresAccessor:
     async def _on_connect(self, application: web.Application):
         from app.store.database.models import db
 
-        # self.config = application["config"]["postgres"]
-        # if self.config["require_ssl"]:
-        #     ctx = ssl.create_default_context(cafile="")
-        #     ctx.check_hostname = False
-        #     ctx.verify_mode = ssl.CERT_NONE
-        #     await db.set_bind(self.config["database_url"], ssl=ctx)
-        # else:
-        #     await db.set_bind(self.config["database_url"])
         self.db = db
         await self.db.set_bind(
             URL(
